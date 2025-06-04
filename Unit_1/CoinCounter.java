147b06fee564e9c510d76e502297e16f35df8f84
Yohan Pereira Da Costa <ypcmassaruti@gmail.com>
2025-06-04 14:59:21 +0100

    Create CoinCounter.java

---
+++ Unit_1/CoinCounter.java
@@ -0,0 +1,12 @@
+package sandbox;
+
+public class CoinCounter {
+
+	public static void main(String[] args) {
+		for(int coin = 1; coin <=10; coin++) {
+			System.out.println("Coin " + coin + " counted");
+		}
+
+	}
+
+}
