8db0dd9a4a9747c337913ee0671867cbbecb3745
Yohan Pereira Da Costa <ypcmassaruti@gmail.com>
2025-06-04 15:03:48 +0100

    Create WaterBottle.java

---
+++ Unit_1/WaterBottle.java
@@ -0,0 +1,14 @@
+package sandbox;
+
+public class WaterBottle {
+
+	public static void main(String[] args) {
+		int water = 0;
+		while(water < 1000) {
+			water+=250;
+			System.out.println("Water: "+water+"ml");
+		}
+
+	}
+
+}
