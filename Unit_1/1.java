ac4918bbe5b9359b336b8854c145ea879cabc3c6
Yohan Pereira Da Costa <ypcmassaruti@gmail.com>
2025-06-04 14:47:41 +0100

    Update 1.java

--- Unit_1/1.java
+++ Unit_1/1.java
@@ -1,5 +1,24 @@
+// If statement
 if(total >= 70){
 	System.out.println("first");
 
 }
 System.out.println("Goodbye");
+
+//If-Else statement
+if(grade > 39){
+	System.out.prinln("Pass");
+} else {
+	System.out.prinln("Fail");
+}
+System.out.println("Thank you");
+
+//If, else-if, else statement
+if (score >= 70){
+	System.out.print("Gold");
+} else if (score >= 40){
+	System.out.print("Silver");
+}else{
+	System.out.print("Bronze");
+}
+System.out.println("Congratulations");
