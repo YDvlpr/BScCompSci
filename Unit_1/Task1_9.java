3a68a07948f515e77f9f8709873774b20cddbcf7
Yohan Pereira Da Costa <ypcmassaruti@gmail.com>
2025-06-04 14:49:36 +0100

    Create Task1_9.java

---
+++ Unit_1/Task1_9.java
@@ -0,0 +1,10 @@
+// Create two integer variables a & b, and output their sum of both
+package sandbox;
+public class Task1_9 {
+	public static void main(String[] args) {
+		int a = 7, b = 2;
+		System.out.println(a+b);
+	}
+}
+
+// result = 9
